69854a8e6c5bdfc16d75894899a960d5557cca5b
Cássia Queiroz <105453035+cassiaqueiroz@users.noreply.github.com>
2024-06-01 00:02:25 -0300

    first commit

---
+++ template-materialui/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import Menu from "./pages/Menu/menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ template-materialui/src/pages/Menu/menu.jsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250, backgroundColor: '#fff' }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon sx={{ color: "#15fde9" }}>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ color: "#361670" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon sx={{ color: "#15fde9" }}>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ color: "#361670" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button 
+        onClick={toggleDrawer(true)} 
+        sx={{ margin: '20px', border: '1px solid #15fde9', borderRadius: '50px', backgroundColor: '#15fde9', color: '#361670' }}
+      >
+        Menu
+      </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)} sx={{ '& .MuiDrawer-paper': { backgroundColor: '#fff' } }}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
